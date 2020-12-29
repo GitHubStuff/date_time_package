859c355c1a6af66860bb6a0fcbcaf06cb6508968
Steven Smith <github@icodeforyou.com>
2020-12-29 18:15:38 -0500

    #Deprecated

--- picker_example/lib/modular/app_module.dart
+++ picker_example/lib/modular/app_module.dart
@@ -1,23 +1,22 @@
 import 'package:date_time_package/picker/models/picker_modular/picker_modular_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-
-import 'app_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart' as M;
 
 import '../resources/constants.dart' as Constants;
 import '../modules/initial_module.dart';
+import 'app_bloc.dart';
 import 'app_widget.dart';
 
-class AppModule extends MainModule {
+class AppModule extends M.MainModule {
   @override
-  List<Bind> get binds => [
-        Bind((i) => AppBloc()),
-        Bind((i) => PickerModularBloc()),
+  List<M.Bind> get binds => [
+        M.Bind((i) => AppBloc()),
+        M.Bind((i) => PickerModularBloc()),
       ];
 
   @override
-  List<Router> get routers => [
-        Router(
+  List<M.Router> get routers => [
+        M.Router(
           Constants.initalRoute,
           module: InitialModule(),
         ),
